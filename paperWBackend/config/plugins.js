b7289a303d974f7e7fa5a0b9098ff43a9c45b4fa
vivianosorio <71792220+Vvan31@users.noreply.github.com>
2023-11-30 00:01:58 -0800

    Email send when visitor register

---
+++ paperWBackend/config/plugins.js
@@ -0,0 +1,19 @@
+module.exports = ({ env }) => ({
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.google.com'),
+          port: env('SMTP_PORT', 587),
+          auth: {
+            user: env('SMTP_USERNAME', 'apw373613@gmail.com'),
+            pass: env('SMTP_PASSWORD', 'apwAdmin10'),
+          },
+        },
+        settings: {
+          defaultFrom: 'apw373613@gmail.com',
+          defaultReplyTo: 'apw373613@gmail.com',
+        },
+      },
+    },
+  });
--- paperWBackend/src/api/visitor/services/visitor.js
+++ paperWBackend/src/api/visitor/services/visitor.js
@@ -1,9 +1,43 @@
 'use strict';
 
-/**
- * visitor service
- */
-
 const { createCoreService } = require('@strapi/strapi').factories;
+const nodemailer = require('nodemailer');
 
-module.exports = createCoreService('api::visitor.visitor');
+module.exports = createCoreService('api::visitor.visitor', {
+  lifecycles: {
+    async afterCreate(data) {
+      const { email, ...rest } = data;
+
+      // Send an email to the new visitor
+      await sendEmail(email, 'Welcome to the Exhibition', 'Thank you for registering!', rest);
+    },
+  },
+});
+
+async function sendEmail(to, subject, text, data) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 587,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  const message = {
+    from: process.env.SMTP_USERNAME, // Or use a default 'from' address
+    to,
+    subject,
+    text,
+    // You can include additional data in the email body if needed
+    html: `<p>Additional information: ${JSON.stringify(data)}</p>`,
+  };
+
+  try {
+    await transporter.sendMail(message);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+}
