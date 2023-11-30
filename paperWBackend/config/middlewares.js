75a5f16686dc89c55eece6cca8717cf102679477
vivianosorio <71792220+Vvan31@users.noreply.github.com>
2023-11-30 00:16:12 -0800

    added middle ware for authentication

--- paperWBackend/config/middlewares.js
+++ paperWBackend/config/middlewares.js
@@ -2,6 +2,7 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  'strapi::jwt',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
--- paperWBackend/config/server.js
+++ paperWBackend/config/server.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://paperworld-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  settings: {
+    cors: {
+      origin: ['http://expo.apw.enterprises', 'https://expo.apw.enterprises', 'http://paperworld-production.up.railway.app/', 'https://paperworld-production.up.railway.app/'],
+    },
   },
 });
