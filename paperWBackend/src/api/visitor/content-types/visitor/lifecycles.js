f5a69d6f22f03b15056f461332445915dbc44f54
vivianosorio <71792220+Vvan31@users.noreply.github.com>
2023-11-30 23:51:51 -0800

    Template email

--- paperWBackend/src/api/visitor/content-types/visitor/lifecycles.js
+++ paperWBackend/src/api/visitor/content-types/visitor/lifecycles.js
@@ -3,16 +3,20 @@ module.exports = {
         const { result } = event;
 
         try{
-            console.log("Email sending...");
+            console.log("Email sending to...");
             console.log(result.email);
             await strapi.plugins['email'].services.email.send({
-              to: 'v31.osorio@gmail.com',
+              to: result.email,
               from: 'a.estrada@apw.enterprises', 
               cc: 'a.estrada@apw.enterprises',
               replyTo: 'a.estrada@apw.enterprises',
               subject: 'The Strapi Email plugin worked successfully',
-              text: 'Hello world! ' + result.email, 
-              html: 'Hello world!', 
+              /* text: 'Hello world! ' + result.email, 
+              html: 'Hello world!',  */
+              templateId: process.env.SENDGRID_VISITOR_TEMPLATE_ID,
+              dynamicTemplateData: {
+                name: result.name
+              }
             })
         } catch(err) {
             console.log(err);
