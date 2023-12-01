0db713401ece8cdeb8ea4c99e5996b0abc8fbcb7
vivianosorio <71792220+Vvan31@users.noreply.github.com>
2023-12-01 01:51:58 -0800

    Update lifecycles.js

--- paperWBackend/src/api/exhibitor/content-types/exhibitor/lifecycles.js
+++ paperWBackend/src/api/exhibitor/content-types/exhibitor/lifecycles.js
@@ -1,33 +1,30 @@
-
 const fs = require('fs').promises;
 const path = require('path');
 const qr = require('qr-image');
 
 module.exports = {
-    async afterCreate(event) {    // Connected to "Save" button in admin panel
+    async afterCreate(event) {
         const { result } = event;
-        
-        try{
+
+        try {
             console.log("Email sending to...");
             console.log(result.email);
+
             const qrData = {
                 slug: result.slug,
                 company: result.company,
-                spot: result.spot.name,
+                spot: result.spot,
                 email: result.email,
                 country: result.countryexhibitor,
                 name: result.name,
                 rfc: result.rfc,
-            }
-           
+            };
+
             const qrCode = qr.image(JSON.stringify(qrData), { type: 'png' });
             const qrCodeFilePath = `./public/uploads/${result.id}-qrcode.png`;
 
             // Save QR code image to file
-            qrCode.pipe(fs.createWriteStream(qrCodeFilePath));
-
-            // Read the saved QR code image as a buffer
-            const qrCodeBuffer = await fs.readFile(qrCodeFilePath);
+            await fs.writeFile(qrCodeFilePath, qrCode.pipe(new (require('stream').PassThrough)()));
 
             await strapi.plugins['email'].services.email.send({
                 to: result.email,
@@ -36,15 +33,17 @@ module.exports = {
                 replyTo: 'a.estrada@apw.enterprises',
                 subject: 'The Strapi Email plugin worked successfully',
                 templateId: process.env.SENDGRID_EXHIBITOR_TEMPLATE_ID,
-                dynamicTemplateData: {
-                    name: result.company,
-                    spot: result.spot.name,
-                    qrCode: {
-                        content: qrCodeBuffer.toString('base64'),
+                attachments: [
+                    {
+                        content: await fs.readFile(qrCodeFilePath, 'base64'),
                         filename: `${result.id}-qrcode.png`,
                         type: 'image/png',
                         disposition: 'attachment',
                     },
+                ],
+                dynamicTemplateData: {
+                    name: result.company,
+                    spot: result.spot.name,
                 },
             });
 
@@ -52,11 +51,9 @@ module.exports = {
             await fs.unlink(qrCodeFilePath);
 
         } catch (err) {
-            console.log(err);
+            console.error(err);
             console.log("Email not sent");
             console.log(JSON.stringify(err, null, 2));
         }
     }
-}
-
-
+};
