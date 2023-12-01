397e927e771a44cdf8957f5db013478a4fe0758d
vivianosorio <71792220+Vvan31@users.noreply.github.com>
2023-12-01 01:40:53 -0800

    Update lifecycles.js

--- paperWBackend/src/api/exhibitor/content-types/exhibitor/lifecycles.js
+++ paperWBackend/src/api/exhibitor/content-types/exhibitor/lifecycles.js
@@ -1,4 +1,6 @@
 
+const fs = require('fs').promises;
+const path = require('path');
 const qr = require('qr-image');
 
 module.exports = {
@@ -11,42 +13,50 @@ module.exports = {
             const qrData = {
                 slug: result.slug,
                 company: result.company,
-                spot: result.spot,
+                spot: result.spot.name,
                 email: result.email,
                 country: result.countryexhibitor,
                 name: result.name,
                 rfc: result.rfc,
             }
+           
             const qrCode = qr.image(JSON.stringify(qrData), { type: 'png' });
-            const qrCodeBuffer = qrCode.pipe(require('fs').createWriteStream(`./public/uploads/${result.id}-qrcode.png`));
+            const qrCodeFilePath = `./public/uploads/${result.id}-qrcode.png`;
+
+            // Save QR code image to file
+            qrCode.pipe(fs.createWriteStream(qrCodeFilePath));
+
+            // Read the saved QR code image as a buffer
+            const qrCodeBuffer = await fs.readFile(qrCodeFilePath);
 
             await strapi.plugins['email'].services.email.send({
-              to: result.email,
-              from: 'a.estrada@apw.enterprises', 
-              cc: 'a.estrada@apw.enterprises',
-              replyTo: 'a.estrada@apw.enterprises',
-              subject: 'The Strapi Email plugin worked successfully',
-              templateId: process.env.SENDGRID_EXHIBITOR_TEMPLATE_ID,
-              dynamicTemplateData: {
-                name: result.name,
-                spot: result.spot,
-                qrCode: `./public/uploads/${result.id}-qrcode.png`,
-              },
-           /*    attachments: [
-                {
-                  content: qrCodeBuffer.toString('base64'),
-                  filename: `${result.id}-qrcode.png`,
-                  path: `./public/uploads/${result.id}-qrcode.png`,
-                  encoding: 'base64',
-                  type: 'image/png',
-                  disposition: 'attachment',
+                to: result.email,
+                from: 'a.estrada@apw.enterprises',
+                cc: 'a.estrada@apw.enterprises',
+                replyTo: 'a.estrada@apw.enterprises',
+                subject: 'The Strapi Email plugin worked successfully',
+                templateId: process.env.SENDGRID_EXHIBITOR_TEMPLATE_ID,
+                dynamicTemplateData: {
+                    name: result.company,
+                    spot: result.spot.name,
+                    qrCode: {
+                        content: qrCodeBuffer.toString('base64'),
+                        filename: `${result.id}-qrcode.png`,
+                        type: 'image/png',
+                        disposition: 'attachment',
+                    },
                 },
-              ], */
-            })
-        } catch(err) {
+            });
+
+            // Remove the temporary QR code image file
+            await fs.unlink(qrCodeFilePath);
+
+        } catch (err) {
             console.log(err);
             console.log("Email not sent");
             console.log(JSON.stringify(err, null, 2));
         }
     }
 }
+
+
