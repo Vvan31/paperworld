3e81f2a94493dacbb47aacf55d347bb234ec84a5
VivianOsorio <71792220+Vvan31@users.noreply.github.com>
2024-04-26 00:31:27 -0700

    navBar for mobile view fixed

--- PaperWorld/src/components/navBar2.jsx
+++ PaperWorld/src/components/navBar2.jsx
@@ -1,17 +1,15 @@
-import  { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import data from '../data.json';
 import Tooltip from '@mui/material/Tooltip';
-
-import "./navBar2.css"
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import logo from '../assets/apwLogo.svg';
-import menu from '../assets/menu.svg';
+import './navBar2.css';
 
 const NavBar2 = ({ language }) => {
   const navigate = useNavigate();
-  const { navBar } = data[language]; 
+  const { navBar } = data[language];
   const [content, setContent] = useState(getInitialContent());
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const storedLanguage = localStorage.getItem('language');
@@ -24,9 +22,6 @@ const NavBar2 = ({ language }) => {
 
   useEffect(() => {
     localStorage.setItem('language', content === data.esp.navBar ? 'esp' : 'eng');
-    const storedLanguage = localStorage.getItem('language');
-    console.log("stored language: ", storedLanguage);
-
   }, [content]);
 
   function getInitialContent() {
@@ -35,55 +30,63 @@ const NavBar2 = ({ language }) => {
   }
 
   const handlePageChange = (selectedPage) => {
-    navigate(`/${selectedPage}`); 
+    navigate(`/${selectedPage}`);
+    setIsMenuOpen(false); // Close the menu after navigating
   };
 
-    return (
-        <nav>
-            <div className="navWrapper">
-                <div className="logoNavBar">
-                    <a href="" onClick={() => handlePageChange('')}>
-                        <img src={logo} alt="APW Logo" width={'70px'} />
-                    </a>
-                </div>
-                <input type="radio" name="slider" id="menu-btn" />
-                <input type="radio" name="slider" id="close-btn" />
-                <ul className="nav-links">
-                    <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
-                    <li>
-                        <a className="desktop-item">{navBar.visitor.title}  </a>
-                        <input type="checkbox" id="showDrop" />
-                        <label htmlFor="showDrop" className="mobile-item">{navBar.visitor.title} </label>
-                        <ul className="drop-menu">
-                            <li><a href=""  onClick={() => handlePageChange('register')}>{navBar.visitor.register}</a></li>
-                            <li><a href="" onClick={() => handlePageChange('hotel')}>{navBar.visitor.hotels}</a></li>
-                            <li><a href="" onClick={() => handlePageChange('schedule')}>{navBar.visitor.program}</a></li>
-                            <li><a href="" onClick={() => handlePageChange('mexico')}>{navBar.visitor.mexico}</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a className="desktop-item">{navBar.exhibitor.title} </a>
-                        <input type="checkbox" id="showDrop2" />
-                        <label htmlFor="showDrop2" className="mobile-item">{navBar.exhibitor.title} </label>
-                        <ul className="drop-menu bigger">
-                            <li><a href="" onClick={() => handlePageChange('expositor')}>{navBar.exhibitor.whyExhibitor}</a></li>
-                            <li><a href="" onClick={() => handlePageChange('scheduleExhibitor')}>{navBar.visitor.program}</a></li>
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
-                            {/* <li><a href="" onClick={() => handlePageChange('mexico')}>{navBar.exhibitor.howRegister}</a></li> */}
-                            <li disabled={true}>
-                            <Tooltip title={navBar.exhibitor.listMessage} arrow>
-                              <a  className='list'>{navBar.exhibitor.list}</a>
-                            </Tooltip>
-                              </li>
-                        </ul>
-                    </li>
-                    <li><a href="" onClick={() => handlePageChange('map')}>{navBar.map}</a></li>
-                    <li><a href="" onClick={() => handlePageChange('contact')}>{navBar.contact}</a></li>
-                </ul>
-                <label htmlFor="menu-btn" className="btn menu-btn"><i className="fas fa-bars"></i></label>
-            </div>
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="navWrapper">
+        <div className="logoNavBar">
+          <a href="" onClick={() => handlePageChange('')}>
+            <img src={logo} alt="APW Logo" width={'70px'} />
+          </a>
+        </div>
+        <input type="checkbox" name="slider" id="menu-btn" checked={isMenuOpen} onChange={toggleMenu} />
+        <input type="checkbox" name="slider" id="close-btn" className="close-btn" onChange={toggleMenu} />
+        {/* <div className="hamburger-menu">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div> */}
+        <ul className="nav-links">
+          <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
+          <li>
+            <a className="desktop-item">{navBar.visitor.title} </a>
+            <input type="checkbox" id="showDrop" />
+            <label htmlFor="showDrop" className="mobile-item">{navBar.visitor.title} </label>
+            <ul className="drop-menu">
+              <li><a href="" onClick={() => handlePageChange('register')}>{navBar.visitor.register}</a></li>
+              <li><a href="" onClick={() => handlePageChange('hotel')}>{navBar.visitor.hotels}</a></li>
+              <li><a href="" onClick={() => handlePageChange('schedule')}>{navBar.visitor.program}</a></li>
+              <li><a href="" onClick={() => handlePageChange('mexico')}>{navBar.visitor.mexico}</a></li>
+            </ul>
+          </li>
+          <li>
+            <a className="desktop-item">{navBar.exhibitor.title} </a>
+            <input type="checkbox" id="showDrop2" />
+            <label htmlFor="showDrop2" className="mobile-item">{navBar.exhibitor.title} </label>
+            <ul className="drop-menu bigger">
+              <li><a href="" onClick={() => handlePageChange('expositor')}>{navBar.exhibitor.whyExhibitor}</a></li>
+              <li><a href="" onClick={() => handlePageChange('scheduleExhibitor')}>{navBar.visitor.program}</a></li>
+              <li disabled={true}>
+                <Tooltip title={navBar.exhibitor.listMessage} arrow>
+                  <a className='list'>{navBar.exhibitor.list}</a>
+                </Tooltip>
+              </li>
+            </ul>
+          </li>
+          <li><a href="" onClick={() => handlePageChange('map')}>{navBar.map}</a></li>
+          <li><a href="" onClick={() => handlePageChange('contact')}>{navBar.contact}</a></li>
+        </ul>
+        <label htmlFor="menu-btn" className="btn menu-btn"><i className="fas fa-bars"></i></label>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar2;
